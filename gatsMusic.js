f897da1fdc2add07ae37cbfc020cdea462705a98
Brendan Pierce <brendan_p@beezwax.net>
2020-03-06 22:21:24 -0800

    upgraded audio quality

--- gatsMusic.js
+++ gatsMusic.js
@@ -77,7 +77,7 @@ class GatsMusic {
             .play(
                 ytdl(ytLink, {
                     filter: 'audioonly',
-                    quality: 251,
+                    quality: 'highestaudio',
                 })
                 .on('info', info => {
                     this._sendNowPlayingText(msg, info.player_response.videoDetails.title);
